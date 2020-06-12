577d3c4edd5facea1f426271a7c7bd64716b0b17
markoaldred <markoaldred@gmail.com>
2020-06-12 22:23:42 +0930

    Added Scrollbar to Checklist Box
    Added Scrollbar to listbox and added the delete multiple items in listbox

--- gui.py
+++ gui.py
@@ -10,30 +10,43 @@ root.geometry("400x700")
 #State variables [testing diff]
 
 #Create and grid the outer content frame
-c = ttk.Frame(root, padding=(5, 5, 12, 0))
+c = ttk.Frame(root, padding=(5, 5, 12, 0)) #
 c.grid(column=0, row=0, sticky=(N,W,E,S))
 root.grid_columnconfigure(0, weight=1)
 root.grid_rowconfigure(0, weight=1)
-c.columnconfigure(0, weight=1)
-c.rowconfigure(1, weight=1)
+#c.columnconfigure(0, weight=1)
+#c.rowconfigure(1, weight=1)
 
-#create widgets
-checklists_listbox = Listbox(c, height=5)
+#create listbox and scroll bar
+checklist_scrollbar = Scrollbar(c, orient=VERTICAL)
+#can set listbox SINGLE, MULTIPLE, BROWSE, EXTENDED
+checklists_listbox = Listbox(c, yscrollcommand=checklist_scrollbar.set, selectmode=EXTENDED)
+
+#cofigure scrollbar
+checklist_scrollbar.config(command=checklists_listbox.yview)
+
+
 #checklists_label = ttk.Label(c, text="Checklists")
 
 #grid all widgets
-checklists_listbox.grid(column=0, row=1, padx=20, pady=(0,20), sticky=(N,S,E,W))
+#checklists_listbox.grid(column=0, row=1, padx=20, pady=(0,20), sticky=(N,S,E,W))
+#checklist_scrollbar.pack(side=RIGHT, fill=Y)
+#checklists_listbox.pack(pady=15)
+checklist_scrollbar.grid(column=1, row=0, sticky=(N,S))
+checklists_listbox.grid(column=0, row=0, ipadx=120, ipady=200)
+
+
 #checklists_label.grid(column=0, row=0, padx=20, pady=(20,0), sticky=(N,E,W,S))
 
 # Add list of items to listbox
-my_list = ["One", "Two", "Three"]
+my_list = ["One", "Two", "Three", "abc", "asfkas", "ahakfs", "ahsoufc", "nafbwi"]
 
 for item in my_list:
 	checklists_listbox.insert(0, item)
 
 def delete():
-	checklists_listbox.delete(ANCHOR)
-	my_label.config(text='')
+	for item in reversed(checklists_listbox.curselection()):
+		checklists_listbox.delete(item)
 
 def select():
 	my_label.config(text=checklists_listbox.get(ANCHOR))
@@ -59,8 +72,15 @@ def delete_all():
 	checklists_listbox.delete(0, END)
 
 
+def select_all():
+	result = ''
+	for item in checklists_listbox.curselection():
+			result = result + str(checklists_listbox.get(item)) + '\n'
+	my_label.config(text=result)
+
+
 global my_label
-my_label = Label(c, text='')
+my_label = Label(root, text='')
 my_label.grid(column=0, row =2, pady=20)
 
 #Create Checklists right click menu
@@ -71,7 +91,7 @@ m.add_command(label="New", command=new_popup)
 m.add_command(label="Delete", command=delete)
 m.add_command(label="Clear All", command=delete_all)
 m.add_command(label="Rename")
-m.add_command(label="Select", command=select)
+m.add_command(label="Select", command=select_all)
 
 #Create Pop up Event at current mouse position
 def do_popup(event):
