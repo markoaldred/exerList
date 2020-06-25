8d79a5502376f9db4ef2de5a87a95728d1d64d75
markoaldred <markoaldred@gmail.com>
2020-06-26 00:30:59 +0930

    Added Task menu with table mod functionality
    Task menu has add, edit, delete, back functionality added.  Also added edit functionality to checklist which opens the task menu.
    
    Copied most of the code from the Checklist table.  Should be updated to use a menu decorate to reduce code.

--- cui.py
+++ cui.py
@@ -43,10 +43,45 @@ def checklist_menu():
 		id_no = database.Checklist.find_id(item_no=checklist_no)
 		database.Checklist.delete_item(id=id_no)
 		checklist_menu()
+	if i == 'e':
+		checklist_no = int(input('Item No: '))
+		id_no = database.Checklist.find_id(item_no=checklist_no)
+		task_menu(id_no)
 
+def task_menu(checklist_id):
+	query = database.session.query(database.Task).filter(checklist_id==checklist_id).all()
+	clear()
+	c1 = []
+	c2 = []
+	for entry in query:
+		c1.append(entry.id)
+		c2.append(entry.description)
+	df = pd.DataFrame({'id' : c1,
+						'description' : c2})
+	print((tabulate(df, headers='keys', tablefmt='psql')))
+	actions('ndeb')
+	i = input('Action: ')
+	if i == 'n':
+		new_name = input('Task: ')
+		#next_item=database.next_item_no(database.Checklist)
+		database.Task.add_item(checklist_id=checklist_id, description=new_name)
+		task_menu(checklist_id)
+	if i == 'e':
+		item_no = int(input('id: '))
+		id_no = database.Task.find_id(id=item_no)
+		new_name =input('Rename Task: ')
+		database.Task.update_item(id=id_no, description=new_name)
+		task_menu(checklist_id)
+	if i == 'd':
+		item_no = int(input('id: '))
+		id_no = database.Task.find_id(id=item_no)
+		database.Task.delete_item(id=id_no)
+		task_menu(checklist_id)
+	if i == 'b':
+		checklist_menu()
 
 def actions(menu_items):
-	actions = {'n':'new', 'e': 'edit', 'd':'delete', 'r':'rename', 'u':'use'}
+	actions = {'n':'new', 'e': 'edit', 'd':'delete', 'r':'rename', 'u':'use', 'b':'back'}
 	print('_______________________________________\n')
 
 	for item in menu_items:
